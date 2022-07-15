c565a2ae4f92298ea98f2b9112b180377b61681b
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-07-15 16:09:41 +0300

    task 3

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,4 @@
+const ul = document.querySelector("ul");
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +13,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const createLiFromArray = (array) =>
+  array.map((el) => `<li>${el}</li>`).join("");
+const list = createLiFromArray(images);
+
+ul.insertAdjacentHTML("beforeend", list);
+console.log(createLiFromArray(images));
