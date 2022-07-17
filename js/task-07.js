d1c2a1ba63fbba0d5f7d2e10e34456278d3c9533
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-07-18 00:04:08 +0300

    task07
    07

---
+++ js/task-07.js
@@ -0,0 +1,9 @@
+const input = document.querySelector("input");
+const span = document.querySelector("#text");
+console.log(input);
+console.log(span);
+input.addEventListener("input", (event) => {
+    textEl.style.fontSize = event.currentTarget.value;
+})
+console.log(textEl.style.fontSize);
+console.log(event.currentTarget.value);
