03b3b68a142463b2065e0d2d1b86f31fe2e2aba2
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-07-19 01:06:50 +0300

    task09
    09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const changeColor = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+changeColor.addEventListener("click", changeBackgroundColor);
+console.dir(color);
+function changeBackgroundColor() {
+  
+  color.style.backgroundColor = getRandomHexColor;
+    
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
