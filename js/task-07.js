f3da7a99d6d51be443abd49b4037894d47785611
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-07-18 16:29:54 +0300

    task08
    done

--- js/task-07.js
+++ js/task-07.js
@@ -4,6 +4,8 @@ console.log(input);
 console.log(span);
 input.addEventListener("input", (event) => {
     textEl.style.fontSize = event.currentTarget.value;
+    console.dir(input);
+    
 })
 console.log(textEl.style.fontSize);
 console.log(event.currentTarget.value);
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    const formEl = event.currentTarget.elements;
+    const email = formEl.email.value;
+    const password = formEl.password.value;
+    if (email === "" || password === "") {
+        alert('порожне поле!');
+        form.reset();
+        return
+    }
+    else {  
+    const formData = new FormData(event.currentTarget);
+   formData.forEach((value, name) => {
+       console.log('onFormSubmit -> name', name);
+       console.log('onFormSubmit -> value', value);
+      
+    });
+      form.reset();  
+}
+    
+}
