594d199f3348f44447b69aa3038579da00391311
SerhiiTsukanov <94555214+SerhiiTsukanov@users.noreply.github.com>
2022-07-19 13:03:48 +0300

    task09
    done

--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,30 @@
-const changeColor = document.querySelector(".change-color");
-const color = document.querySelector(".color");
-changeColor.addEventListener("click", changeBackgroundColor);
-console.dir(color);
-function changeBackgroundColor() {
-  
-  color.style.backgroundColor = getRandomHexColor;
-    
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+// const body = document.querySelector(".body");
+// const changeColor = document.querySelector(".change-color");
+// const color = document.querySelector(".color");
+// changeColor.addEventListener("click", changeBackgroundColor);
+// console.dir(color);
+// function changeBackgroundColor() {
+//   const newColor = getRandomHexColor()
+//   body.style.background = newColor;
+//   color.textContent = `${newColor}`;
+
+// }
+
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)}`;
+}
+const body = document.querySelector("body");
+const button = document.querySelector(".change-color");
+let color = document.querySelector(".color");
+button.addEventListener("click", changeColor);
+function changeColor() {
+  const newColor = getRandomHexColor()
+  body.style.backgroundColor = newColor;
+  color.textContent = `${newColor}`;
 }
